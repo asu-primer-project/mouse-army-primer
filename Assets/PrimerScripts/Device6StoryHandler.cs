1bb008acc22262ea5ac34832cee6fda5e699d7be
pradeephrish <chaudharipradeep9@gmail.com>
2015-03-18 17:47:26 -0700

    Device 6 ruff plan

---
+++ Assets/PrimerScripts/Device6StoryHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Device6StoryHandler : MonoBehaviour {
+
+	//2D table to Store all the Frames of Whole Canvas
+	// Coordinates | Text  | Scroll Direction | Level       | Frame Number
+	// 0,0         | Once. | Down             | none        |  1
+	//...
+	//...
+	//0,100        | some. | Right             | Chapter 2   | 7
+
+
+	//CurrentFrame -- Store Current location
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		//some kind of touch / scroll handler
+
+		/*
+		 * 
+		 *  TouchHandler {
+		 * 
+		 *      Function ( InputDirection, CurrentFrame ) {
+		 *            check from 2D table if transition allowed 
+		 *         
+		 *            showFrame -- to update the canvas view  or Move Camera to play frame
+		 *        		
+		 *      }
+		 * 
+		 *   }
+		 *             
+		 *
+		 * }
+		 * 
+		 * 
+		 * /
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
